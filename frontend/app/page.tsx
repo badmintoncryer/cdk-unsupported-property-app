4df5c7c6f91101452cbe06ebedeb77c5a5a34dcf
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-02 13:04:32 +0900

    style: update description

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,17 +4,50 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
 			<div className="container mx-auto px-4">
-				<h1 className="text-4xl font-bold text-center my-6 text-white">
-					AWS CDK Unsupported Properties for L2 by {""}
-					<a
-						href="https://github.com/badmintoncryer"
-						className="text-blue-400 hover:text-blue-600"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						@badmintoncryer
-					</a>
-				</h1>
+				<div className="bg-gray-800 text-white p-6 rounded-lg shadow mb-6">
+					<h1 className="text-lg font-bold mb-2">
+						AWS CDK L2 Constructs Unsupported Arguments {""}
+						<a
+							href="https://github.com/badmintoncryer"
+							className="text-blue-400 hover:text-blue-600"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							@badmintoncryer
+						</a>
+					</h1>
+					<p className="mb-4">
+						This page displays a list of unsupported arguments in AWS CDK L2
+						constructs. For more details, visit{" "}
+						<a
+							href="https://speakerdeck.com/badmintoncryer/cdkkontoribiyutonozui-chu-nobi-woyue-eyou-jian-dan-issuenojian-tukefang"
+							className="text-blue-400 hover:text-blue-600"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							this reference
+						</a>
+						.
+					</p>
+					<p className="italic">
+						Note: The following example, where arguments are indirectly passed,
+						is <span className="text-red-500">not</span> currently supported.
+						Although it might appear as unsupported, it is actually handled.
+					</p>
+					<pre className="bg-gray-700 p-4 rounded">
+						<code>
+							{`const props = {
+  hoge: 'hoge',
+  fuga: 123,
+};
+
+new CfnConstruct(scope, 'Resource', {
+  ...props,
+});`}
+						</code>
+					</pre>
+				</div>
+
 				{missingProperties.map((item, index) => (
 					<div
 						key={index}
