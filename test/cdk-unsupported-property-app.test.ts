aa0cba7c100294bbe9f3ec9f334fac26b39951b5
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-02 10:51:43 +0900

    Initial commit

---
+++ test/cdk-unsupported-property-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkUnsupportedPropertyApp from '../lib/cdk-unsupported-property-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-unsupported-property-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkUnsupportedPropertyApp.CdkUnsupportedPropertyAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
