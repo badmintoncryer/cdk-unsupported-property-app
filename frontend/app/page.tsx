6b094800a97acac04dc723099643945491796171
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-04 09:38:02 +0900

    feat: add tags filter

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,35 @@
+"use client";
+
+import { useState } from "react";
 import missingProperties from "./missingProperties.json";
 
+type MissingProperty = {
+	module: string;
+	name: string;
+	missingProps: string[];
+};
+
 export default function Home() {
+	const [showTags, setShowTags] = useState(false);
+	const initialData: MissingProperty[] = [];
+	// missingPropsから'tags'を削除し、その結果要素数が0になった項目を取り除く
+	const processedData = missingProperties.reduce((acc, item) => {
+		// 'tags'を除外する
+		const filteredProps = showTags
+			? item.missingProps
+			: item.missingProps.filter((prop) => prop !== "tags");
+
+		// filteredPropsが空でなければ、新しい配列に追加
+		if (filteredProps.length > 0) {
+			acc.push({
+				...item,
+				missingProps: filteredProps,
+			});
+		}
+
+		return acc;
+	}, initialData);
+
 	return (
 		<main className="flex flex-col min-h-screen justify-between p-4 sm:p-24 bg-black">
 			<h1 className="text-xl sm:text-3xl font-bold mb-6 text-center">
@@ -16,6 +45,29 @@ export default function Home() {
 			</h1>
 			<div className="container mx-auto">
 				<div className="text-white p-4 rounded-lg shadow mb-6 bg-gray-800">
+					<div className="flex items-center justify-end">
+						<label
+							htmlFor="toggle"
+							className="text-white text-sm sm:text-base mr-4"
+						>
+							Show `tags` Prop
+						</label>
+						<div className="relative inline-block w-10 align-middle select-none transition duration-200 ease-in">
+							<input
+								type="checkbox"
+								name="toggle"
+								id="toggle"
+								className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+								checked={showTags}
+								onChange={() => setShowTags(!showTags)}
+							/>
+							<label
+								htmlFor="toggle"
+								className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+							/>
+						</div>
+					</div>
+
 					<p className="text-sm sm:text-base mb-4">
 						This page displays a list of unsupported properties in AWS CDK L2
 						constructs. For more details, visit{" "}
@@ -70,7 +122,7 @@ new CfnConstruct(scope, 'Resource', {
 					</div>
 				</div>
 
-				{missingProperties
+				{processedData
 					.sort((a, b) => {
 						const moduleCompare = a.module.localeCompare(b.module);
 						if (moduleCompare !== 0) {
@@ -78,17 +130,17 @@ new CfnConstruct(scope, 'Resource', {
 						}
 						return a.name.localeCompare(b.name);
 					})
-					.map((item, index) => (
+					.map((item) => (
 						<div
-							key={index}
+							key={item.module + item.name}
 							className="mb-5 p-4 shadow-lg rounded-lg bg-gray-800"
 						>
 							<h2 className="text-xl sm:text-2xl font-semibold text-white">
 								{item.module} - {item.name}
 							</h2>
 							<ul className="list-disc list-inside text-gray-300">
-								{item.missingProps.map((prop, idx) => (
-									<li key={idx} className="text-lg">
+								{item.missingProps.map((prop) => (
+									<li key={prop} className="text-lg">
 										{prop}
 									</li>
 								))}
